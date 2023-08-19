094cbad35415ff6818f4a6395cfb0a680b303384
Vieira Granzoto <61919043+vieiragranzoto@users.noreply.github.com>
2023-08-19 11:01:22 -0300

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -14,29 +14,16 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-rm -rf yay
 # Remove virtualbox-guest-utils if not needed
 sudo pacman -S --noconfirm virtualbox-guest-utils sway wayland xorg-xwayland waybar swaybg alacritty rofi firefox nemo pulseaudio pulseaudio-alsa alsa-utils pulseaudio-alsa pavucontrol neofetch zsh zsh-completions ttf-fantasque-nerd ttf-jetbrains-mono-nerd grim feh slurp gtk2 gnome-themes-extra adwaita-icon-theme
 
 systemctl enable vboxservice.service
 
-VBoxClient --clipboard
-VBoxClient --draganddrop
-VBoxClient --seamless
-VBoxClient --checkhostversion
-VBoxClient --vmsvga
-
 git clone https://github.com/vieiragranzoto/dotfiles.git
-
-chsh -s /usr/bin/zsh
-
-zsh
-
-q
 
 cd dotfiles
 mv .zshrc ~
-mv .gtk-2.0 ~
+mv .gtkrc-2.0 ~
 mv .config ~
 mv .mozilla ~
 mv Images ~
@@ -51,4 +38,8 @@ yay --noconfirm -S autotiling
 
 pulseaudio -D
 
+chsh -s /usr/bin/zsh
+
 rm -rf archlinux
+
+reboot
